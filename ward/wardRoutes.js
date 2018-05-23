1f6023da2d9dd95bb0507640f82825112c6d9118
Aaquiff Rauf <aaquiff@gmail.com>
2018-05-24 00:24:32 +0530

    init

--- ward/wardRoutes.js
+++ ward/wardRoutes.js
@@ -8,31 +8,36 @@ router.use(bodyParser.json())
 router.get('/', (req,res)=> {
     wardController.getAll().then((data) => {
         res.setHeader('Content-Type', 'application/json')
-        res.status(data.status).send(data.wards);
+        res.header('Access-Control-Allow-Origin', '*')
+        res.status(data.status).send(JSON.stringify(data.wards));
     })
 })
 
 router.get('/:id', (req,res)=> {
     wardController.getWard(req.params.id).then((data) => {
         res.setHeader('Content-Type', 'application/json')
+        res.header('Access-Control-Allow-Origin', '*')
         res.status(data.status).send(data.ward);
     })
 })
 
 router.post('/', (req,res) => {
     wardController.insertWard(req.body).then((data) => {
+        res.header('Access-Control-Allow-Origin', '*')
         res.status(data.status).send(data.message);
     })
 })
 
 router.put('/:id', (req,res) => {
     wardController.updateWard(req.body).then((data) => {
+        res.header('Access-Control-Allow-Origin', '*')
         res.status(data.status).send(data.ward);
     })
 })
 
 router.delete('/:id', (req,res) => {
     wardController.deleteWard(req.params.id).then((data) => {
+        res.header('Access-Control-Allow-Origin', '*')
         res.status(data.status).send(data.message);
     })
 })
